1a34a6ddd7d8e4275f64a019c80f70559c12d583
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-07-30 20:41:58 -0300

    feat: add class Curso and Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import br.com.dio.desafio.domain.Curso;
+import br.com.dio.desafio.domain.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        LocalDate data = LocalDate.now();
+
+        Curso curso = new Curso(8, "curso java", "descrição curso java");
+
+        Curso curso1 = new Curso(8, "curso js", "descrição curso js");
+
+        Mentoria mentoria = new Mentoria(data, "descrição mentoria java", "mentoria de java");
+
+        System.out.println(curso);
+        System.out.println(curso1);
+        System.out.println(mentoria);
     }
 }
